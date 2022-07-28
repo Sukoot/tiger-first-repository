e04944bd355a500940ff1040ac347b9e4435dcbf
Sukoot <massouda.s@live.com>
2022-07-28 08:30:20 -0400

    this is tiger practice for api

---
+++ src/test/java/tiger/api/practice/runner/TestRunner.java
@@ -0,0 +1,11 @@
+package tiger.api.practice.runner;
+
+import com.intuit.karate.junit5.Karate;
+
+public class TestRunner {
+	@Karate.Test
+	public Karate runner() {
+		return Karate.run("classpath:features").tags("@End2End");
+	}
+
+}
